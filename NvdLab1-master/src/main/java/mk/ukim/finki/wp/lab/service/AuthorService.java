bd5cf13102f5a0d3b80325adfa435d01c160421b
ivosokoloski <ivo.sokoloski12@gmail.com>
2025-12-03 00:45:50 +0100

    add

--- NvdLab1-master/src/main/java/mk/ukim/finki/wp/lab/service/AuthorService.java
+++
@@ -1,9 +0,0 @@
-package mk.ukim.finki.wp.lab.service;
-
-import mk.ukim.finki.wp.lab.model.Author;
-
-import java.util.List;
-
-public interface AuthorService {
-    public List<Author> findAll();
-}
